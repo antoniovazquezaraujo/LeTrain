489e55f6172365e995476a5c8a5972a70573bea8
Antonio Vazquez <antoniovazquezaraujo@gmail.com>
2026-05-13 11:34:18 +0200

    fix: raise station plate from ground, increase label offset

--- src/main/java/letrain/visitor/gdx3d/InfrastructureRenderer.java
+++ src/main/java/letrain/visitor/gdx3d/InfrastructureRenderer.java
@@ -267,7 +267,7 @@ public class InfrastructureRenderer extends BaseSubRenderer {
         ModelInstance plate = resourceContext.getModelInstance(resourceContext.wagonJewelModel);
         plate.materials.get(0)
                 .set(com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute.createDiffuse(structureColor));
-        plate.transform.setToTranslation(plateMidX, 0.01f, plateMidZ);
+        plate.transform.setToTranslation(plateMidX, 0.03f, plateMidZ);
         plate.transform.rotate(0, 1, 0, plateAngle);
         plate.transform.scale(plateLengthPerp, 0.01f, plateWidth);
         if (alpha < 1.0f) {
@@ -301,7 +301,7 @@ public class InfrastructureRenderer extends BaseSubRenderer {
         labelColor.a = alpha;
 
         String idText = (id >= 0) ? String.valueOf(id) : "?";
-        float labelOffset = boardSize / 2f + 0.01f;
+        float labelOffset = boardSize / 2f + 0.04f;
 
         labels.add(new Gdx3DRenderer.VehicleLabel(
                 new Vector3(centerX + perpX * labelOffset, boardCenterY,
