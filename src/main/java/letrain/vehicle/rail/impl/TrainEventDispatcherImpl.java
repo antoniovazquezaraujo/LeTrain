1f129ff09ea55fefc245f6713377f6b37343bdc2
Antonio Vazquez <antoniovazquezaraujo@gmail.com>
2026-06-02 14:28:28 +0200

    add @Override annotations to TrainEventDispatcherImpl

--- src/main/java/letrain/vehicle/rail/impl/TrainEventDispatcherImpl.java
+++ src/main/java/letrain/vehicle/rail/impl/TrainEventDispatcherImpl.java
@@ -22,6 +22,7 @@ public class TrainEventDispatcherImpl implements TrainEventDispatcher {
         this.coreTrainListeners = new CopyOnWriteArrayList<>();
     }
 
+    @Override
     public List<TrainEventListener> getScriptTrainListeners() {
         if (scriptTrainListeners == null) {
             scriptTrainListeners = new CopyOnWriteArrayList<>();
@@ -29,6 +30,7 @@ public class TrainEventDispatcherImpl implements TrainEventDispatcher {
         return Collections.unmodifiableList(scriptTrainListeners);
     }
 
+    @Override
     public List<TrainEventListener> getCoreTrainListeners() {
         if (coreTrainListeners == null) {
             coreTrainListeners = new CopyOnWriteArrayList<>();
@@ -36,6 +38,7 @@ public class TrainEventDispatcherImpl implements TrainEventDispatcher {
         return Collections.unmodifiableList(coreTrainListeners);
     }
 
+    @Override
     public void addScriptTrainEventListener(TrainEventListener listener) {
         if (scriptTrainListeners == null) {
             scriptTrainListeners = new CopyOnWriteArrayList<>();
@@ -43,12 +46,14 @@ public class TrainEventDispatcherImpl implements TrainEventDispatcher {
         scriptTrainListeners.add(listener);
     }
 
+    @Override
     public void removeScriptTrainEventListener(TrainEventListener listener) {
         if (scriptTrainListeners != null) {
             scriptTrainListeners.remove(listener);
         }
     }
 
+    @Override
     public void addCoreTrainEventListener(TrainEventListener listener) {
         if (coreTrainListeners == null) {
             coreTrainListeners = new CopyOnWriteArrayList<>();
@@ -56,23 +61,27 @@ public class TrainEventDispatcherImpl implements TrainEventDispatcher {
         coreTrainListeners.add(listener);
     }
 
+    @Override
     public void removeCoreTrainEventListener(TrainEventListener listener) {
         if (coreTrainListeners != null) {
             coreTrainListeners.remove(listener);
         }
     }
 
+    @Override
     public void removeAllScriptTrainEventListeners() {
         if (scriptTrainListeners != null) {
             scriptTrainListeners.clear();
         }
     }
 
+    @Override
     public void postLoadInit() {
         scriptTrainListeners = SerializationHelper.ensureListInitializedConcurrent(scriptTrainListeners);
         coreTrainListeners = SerializationHelper.ensureListInitializedConcurrent(coreTrainListeners);
     }
 
+    @Override
     public void notifySpeedChanged(int speed) {
         for (TrainEventListener l : scriptTrainListeners) {
             l.onSpeedChanged(speed);
@@ -82,6 +91,7 @@ public class TrainEventDispatcherImpl implements TrainEventDispatcher {
         }
     }
 
+    @Override
     public void notifySenseChanged(boolean forward) {
         for (TrainEventListener l : scriptTrainListeners) {
             l.onSenseChanged(forward);
@@ -91,26 +101,31 @@ public class TrainEventDispatcherImpl implements TrainEventDispatcher {
         }
     }
 
+    @Override
     public void notifyLink() {
         scriptTrainListeners.forEach(l -> l.onLink(train));
         coreTrainListeners.forEach(l -> l.onLink(train));
     }
 
+    @Override
     public void notifyUnlink() {
         scriptTrainListeners.forEach(l -> l.onUnlink(train));
         coreTrainListeners.forEach(l -> l.onUnlink(train));
     }
 
+    @Override
     public void notifyEnterSensor(boolean isForward) {
         scriptTrainListeners.forEach(l -> l.onSensorEnter(train, isForward));
         coreTrainListeners.forEach(l -> l.onSensorEnter(train, isForward));
     }
 
+    @Override
     public void notifyExitSensor(boolean isForward) {
         scriptTrainListeners.forEach(l -> l.onSensorExit(train, isForward));
         coreTrainListeners.forEach(l -> l.onSensorExit(train, isForward));
     }
 
+    @Override
     public void notifyContact(Point pos, int speed) {
         for (TrainEventListener l : scriptTrainListeners) {
             l.onContact(train, pos, speed);
@@ -120,6 +135,7 @@ public class TrainEventDispatcherImpl implements TrainEventDispatcher {
         }
     }
 
+    @Override
     public void notifyCrash(Point pos, int speed) {
         for (TrainEventListener l : scriptTrainListeners) {
             l.onCrash(train, pos, speed);
