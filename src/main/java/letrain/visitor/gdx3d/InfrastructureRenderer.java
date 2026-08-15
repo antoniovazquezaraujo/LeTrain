98ec6f456821fcc264d12471e0f12083bcbb9ff9
Antonio Vazquez <antoniovazquezaraujo@gmail.com>
2026-08-15 20:10:15 +0200

    Extract magic numbers for z-fighting tweaks into named variables

--- src/main/java/letrain/visitor/gdx3d/InfrastructureRenderer.java
+++ src/main/java/letrain/visitor/gdx3d/InfrastructureRenderer.java
@@ -327,7 +327,9 @@ public class InfrastructureRenderer extends BaseSubRenderer {
         ModelInstance plate = resourceContext.getModelInstance(resourceContext.wagonJewelModel);
         plate.materials.get(0)
                 .set(com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute.createDiffuse(structureColor));
-        plate.transform.setToTranslation(plateMidX, 0.1f, plateMidZ);
+        
+        float cargoPlateElevation = 0.1f;
+        plate.transform.setToTranslation(plateMidX, cargoPlateElevation, plateMidZ);
         plate.transform.rotate(0, 1, 0, plateAngle);
         plate.transform.scale(plateLengthPerp, 0.05f, plateWidth);
         if (alpha < 1.0f) {
@@ -361,7 +363,9 @@ public class InfrastructureRenderer extends BaseSubRenderer {
         labelColor.a = alpha;
 
         String idText = (id >= 0) ? String.valueOf(id) : "?";
-        float labelOffset = boardSize / 2f + 0.05f;
+        
+        float zFightingLabelOffset = 0.05f;
+        float labelOffset = boardSize / 2f + zFightingLabelOffset;
 
         labels.add(new Gdx3DRenderer.VehicleLabel(
                 new Vector3(centerX + perpX * labelOffset, boardCenterY,
