acf2e162122bef36da16e2b1b77fb8a7b3cc8c9a
Antonio Vazquez <antoniovazquezaraujo@gmail.com>
2026-08-17 12:27:19 +0200

    Fix missing java.util.List imports

--- src/main/java/letrain/ground/impl/GroundMap.java
+++ src/main/java/letrain/ground/impl/GroundMap.java
@@ -1,8 +1,10 @@
 package letrain.ground.impl;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
