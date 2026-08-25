7c27cecf5fdbf9fed8ec70d5541c02ea7c51d904
Antonio Vazquez <antoniovazquezaraujo@gmail.com>
2026-08-25 19:17:03 +0200

    Chore: Remove temporary scripts and patch files

--- update_handler.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sed -i '/private int stationId = 0;/a \    private int speedSignalId = 0;\n    private long speedSignalInputTimeout = 0;' src/main/java/letrain/mvp/impl/graphic/Gdx3DInputHandler.java
-
-sed -i "/case 's':/i \\                    case 'g':\n                        if (!model.getSpeedSignals().isEmpty())\n                            model.setMode(Model.GameMode.SPEED_SIGNALS);\n                        return;" src/main/java/letrain/mvp/impl/graphic/Gdx3DInputHandler.java
-
-sed -i '/case SEMAPHORES:/a \            case SPEED_SIGNALS:\n                handleSpeedSignalsInput(stroke);\n                break;' src/main/java/letrain/mvp/impl/graphic/Gdx3DInputHandler.java
-
-sed -i '/private void handleSemaphoreInput(KeyStroke keyEvent) {/r patch_speed_signal_input.txt' src/main/java/letrain/mvp/impl/graphic/Gdx3DInputHandler.java
